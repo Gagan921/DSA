41f1a1a0bf8c35aab60a83ac526a5bdbfe6ad26e
Gagan Arora <63442672+Gagan921@users.noreply.github.com>
2024-04-09 03:28:49 +0530

    Time: 83 ms (66.33%) | Memory: 53 MB (80.87%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -7,17 +7,6 @@ var lengthOfLongestSubstring = function(s) {
     let ans=0;
     let left = 0;
     let right = 0;
-    // for(let i = 0;i<s.length;i++){
-    //     let c  = s.charAt(i);
-    //     if(!st.has(c)){
-    //         st.add(c);
-    //         ans = Math.max(i-left+1, ans);
-    //     }else if(st.has(c)){
-    //         st.delete(s.charAt(left))
-    //         left++;
-    //     }
-    // }
-    // return s.length==0?0:ans;
     for(let i = 0; i < s.length; i++){
         let c = s.charAt(i);
         while (st.has(c)) {
